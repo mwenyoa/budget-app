a70b00232c00cf7523782193c08492df5eec174e
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-06 06:55:54 +0200

    validate categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :transactions, through: :category_transactions
+  has_many :transactions, through: :category_transactions, dependent: :destroy
+  has_one_attached :icon
+
+  # validations
+  validates :name, :icon, presence: true
 end
