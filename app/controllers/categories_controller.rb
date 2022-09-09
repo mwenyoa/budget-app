48a00a9c9912f9596dfbde1a7aad38b3a257a101
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-09 15:50:58 +0200

    Add caegories controller and logic

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,45 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
   load_and_authorize_resource
 
-  def index; end
+  def index
+    @category = Category.order(created_at: :desc).limit(4)
+  end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+  end
 
-  def create; end
+  def create
+    @user = current_user
+    @category = @user.categories.new(category_params)
+    if @category.save
+      flash[:notice] = "Budget ccategory successfully created!"
+      redirect_to user_categories_path(current_user)
+    else
+      flash[:alert] = 'Category could not be created!'
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    if @category.destroy
+      flash[:notice] = 'Category Successfully Deleted!'
+      redirect_to authenticated_root_path
+    else
+      flash[:alert] = 'Error prevented the category from being deleted!'
+      redirect_to authenticated_root_path
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
